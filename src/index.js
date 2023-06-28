3480b1fd5e2b647dbb3a9a2a8710338709a5235f
geekelo <efuelight12@gmail.com>
2023-06-28 22:12:22 +0100

    Add design for front-end

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import _ from 'lodash';
 import printMe from './print.js';
 import './style.css';
+import './media-library/bg-image.jpg';
 
- function component() {
-   const element = document.createElement('div');
-  const btn = document.createElement('button');
+//  function component() {
+//    const element = document.createElement('div');
+//   const btn = document.createElement('button');
 
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-   element.classList.add('hello');
+//    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//    element.classList.add('hello');
 
 
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
+//   btn.innerHTML = 'Click me and check the console!';
+//   btn.onclick = printMe;
 
-  element.appendChild(btn);
+//   element.appendChild(btn);
 
-   return element;
- }
+//    return element;
+//  }
 
- document.body.appendChild(component());
+//  document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,10 @@ const path = require('path');
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   optimization: {
