bd771d672a4236562ccf36ad172a16402460212b
geekelo <efuelight12@gmail.com>
2023-06-29 17:22:34 +0100

    Add post data functionality

--- src/fetchdata.js
+++ src/fetchdata.js
@@ -4,12 +4,9 @@ export default class GetData {
       const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdz/scores';
       const response = await fetch(url);
 
-      if (response.ok) {
         const result = await response.json();
         return result;
-      } else {
-        throw new Error('Retrieving data failed');
-      }
+    
     } catch (error) {
       console.error(error); // Handle or log the error
     }
