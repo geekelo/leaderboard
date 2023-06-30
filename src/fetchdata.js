617970e18902f0354af5916c7f55b224f2b65b8c
geekelo <efuelight12@gmail.com>
2023-06-30 10:32:39 +0100

    Fix linter errors

--- src/fetchdata.js
+++ src/fetchdata.js
@@ -1,12 +1,12 @@
 export default class GetData {
   constructor() {
-    this.fetchData(); // Use 'this' keyword to call the fetchData method
+    this.url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/FKHju9Dj2M8LwqYT9Cgw/scores';
+    this.fetchData();
   }
 
   async fetchData() {
     try {
-      const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/FKHju9Dj2M8LwqYT9Cgw/scores';
-      const response = await fetch(url);
+      const response = await fetch(this.url);
       const result = await response.json();
       return result;
     } catch (error) {
--- src/postData.js
+++ src/postData.js
@@ -2,34 +2,28 @@ export default class SendData {
   constructor() {
     this.playerName = document.querySelector('#user');
     this.playerScore = document.querySelector('#score');
+    this.url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/FKHju9Dj2M8LwqYT9Cgw/scores';
     this.setupListener();
     this.postData();
   }
 
   async postData(playername, playerscore) {
     try {
-      const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/FKHju9Dj2M8LwqYT9Cgw/scores';
-
       const data = {
         score: playerscore,
         user: playername,
       };
 
-      const response = await fetch(url, {
+      const response = await fetch(this.url, {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
       });
-
-      if (response.ok) {
-        const result = await response.json();
-        console.log(result);
-        return result;
-    } 
-
-  } catch (error) {
+      const result = await response.json();
+      return result;
+    } catch (error) {
       return null;
     }
   }
@@ -45,4 +39,3 @@ export default class SendData {
     });
   }
 }
-
